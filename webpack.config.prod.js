08368f37a798eef120e9c1e10bf73bb985b93550
haris18896 <haris18896@gmail.com>
2025-08-26 19:55:57 +0500

    Fix webpack publicPath to use relative paths for Vercel deployment

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
-    publicPath: '/',
+    publicPath: './',
     clean: true,
   },
   module: {
