9725b8b24102aeee4c712a1e15e5d410485839af
haris18896 <haris18896@gmail.com>
2025-08-26 20:31:44 +0500

    deploying to Netlify

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -100,7 +100,7 @@ module.exports = {
       new TerserPlugin({
         terserOptions: {
           compress: {
-            drop_console: true,
+            drop_console: false,
           },
         },
       }),
